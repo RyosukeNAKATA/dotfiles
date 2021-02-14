27ac8190e411a23db0b19634c0eaecde13b175ee
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-02-15 02:18:09 +0900

    Update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,4 @@
 ln -sf /Users/ryosuke/dotfiles/.zshrc ~/.zshrc
 ln -sf /Users/ryosuke/dotfiles/.config ~/.config
 ln -sf /Users/ryosuke/dotfiles/.cache ~/.cache
+ln -sf /Users/ryosuke/dotfiles/.tmux.conf ~/.tmux.conf
