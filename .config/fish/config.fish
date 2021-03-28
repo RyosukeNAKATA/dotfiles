589ad438bdef73ec456d54fd88eb7fce0dabc819
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-03-28 23:42:36 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,7 +14,6 @@ alias ll='exa -lbGF --git'                                             # long li
 alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
 alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
 alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-
 # specialty views
 alias lS='exa -1'                                                              # one column, just names
 alias lt='exa --tree --level=2'                                         # tree
@@ -22,9 +21,10 @@ alias lt='exa --tree --level=2'                                         # tree
 # neovim
 abbr vim 'nvim'
 abbr vi 'nvim'
-
+# jupyter lab
+abbr jl 'jupyter-lab'
 # pipx-upgrade-all
-alias pipx-upgrade-all="pip list -o | tail -n +3 | awk '{ print \$1 }' | xargs pipx install -U"
+alias pip-upgrade-all="pip list -o | tail -n +3 | awk '{ print \$1 }' | xargs pip install -U"
 
 # Created by `userpath` on 2021-02-15 13:14:49
 set PATH $PATH /Users/ryosuke/.local/bin
