a6c6ccab517e97c4c5f23a7f430e7df0ac2fe3cc
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-04-06 11:33:27 +0900

    fix

--- .cache/dein/.cache/init.vim/.dein/installer/install-rust-analyzer.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-setlocal
-set VERSION=2021-01-18
-curl -L -o rust-analyzer-windows.exe "https://github.com/rust-analyzer/rust-analyzer/releases/download/%VERSION%/rust-analyzer-windows.exe"
-
-move rust-analyzer-windows.exe rust-analyzer.exe
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ln -sf /Users/ryosuke/dotfiles/.zshrc ~/.zshrc
 ln -sf /Users/ryosuke/dotfiles/.config ~/.config
-ln -sf /Users/ryosuke/dotfiles/.cache ~/.cache
 ln -sf /Users/ryosuke/dotfiles/.tmux.conf ~/.tmux.conf
