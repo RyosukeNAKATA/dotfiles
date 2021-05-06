99fb813947e783eacdfd6d05f673c3f246877f7f
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-05-06 23:49:26 +0900

    Update SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-ln -sf /home/narchan/dotfiles/.bashrc /home/narchan/.bashrc
-ln -sf /home/narchan/dotfiles/.zshrc /home/narchan/.zshrc
-ln -sf /home/narchan/dotfiles/.psqlrc /home/narchan/.psqlrc
-ln -sf /home/narchan/dotfiles/.config /home/narchan/.config
-ln -sf /home/narchan/dotfiles/.tmux.conf /home/narchan/.tmux.conf
+ln -sP /home/narchan/dotfiles/.bashrc /home/narchan/.bashrc
+ln -sP /home/narchan/dotfiles/.zshrc /home/narchan/.zshrc
+ln -sP /home/narchan/dotfiles/.psqlrc /home/narchan/.psqlrc
+ln -sP /home/narchan/dotfiles/.config /home/narchan/.config
+ln -sP /home/narchan/dotfiles/.tmux.conf /home/narchan/.tmux.conf
