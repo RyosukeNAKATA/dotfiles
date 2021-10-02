03412f4e9f62092e938cd32b4b6eade86b6d4d93
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-10-02 23:09:22 +0900

    change tmux to zellij

--- SETUP.sh
+++ SETUP.sh
@@ -4,4 +4,3 @@ ln -sf /home/narchan/dotfiles/.zshrc /home/narchan/.zshrc
 ln -sf /home/narchan/dotfiles/.psqlrc /home/narchan/.psqlrc
 ln -sf /home/narchan/dotfiles/.tmux.conf /home/narchan/.tmux.conf
 ln -sfn /home/narchan/dotfiles/.config/ /home/narchan/.config/
-
