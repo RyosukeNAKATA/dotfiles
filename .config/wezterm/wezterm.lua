392e85f91cc30aa5d5d206b0b1798035d0a242f7
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2021-12-07 09:09:07 +0900

    fix

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,12 @@ return {
   color_scheme = "iceberg-dark",
   window_background_opacity = 0.9,
   enable_tab_bar = true,
+  window_padding = {
+    left = 15,
+    right = 15,
+    top = 5,
+    bottom = 15,
+  },
   initial_cols = 190,
   initial_rows = 70,
   adjust_window_size_when_changing_font_size = true,
