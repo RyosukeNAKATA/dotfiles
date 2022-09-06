49e1f2cda7ef41e722001abff9f544a9771f6e94
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-09-07 01:13:45 +0900

    Fix indents

--- .config/nvim/plugins/ddu.vim.lua
+++ .config/nvim/plugins/ddu.vim.lua
@@ -1,17 +1,17 @@
 vim.cmd ([[
     call ddu#custom#patch_global({
-		\   'columns': ['icon_filename'],
-		\   'ui': 'filer',
-		\   'sources': [{'name': 'file', 'params': {}}],
-		\   'sourceOptions': {
-		\     '_': {
-		\       'columns': ['icon_filename'],
-		\     },
-		\   },
-		\   'kindOptions': {
-		\     'file': {
-		\       'defaultAction': 'open',
-		\     },
-		\   },
-		\ })
+	\   'columns': ['icon_filename'],
+	\   'ui': 'filer',
+	\   'sources': [{'name': 'file', 'params': {}}],
+	\   'sourceOptions': {
+	\     '_': {
+	\       'columns': ['icon_filename'],
+	\     },
+	\   },
+	\   'kindOptions': {
+	\     'file': {
+	\       'defaultAction': 'open',
+	\     },
+	\   },
+	\ })
 ]])
