b4753f7b087daea0b06ac27d2cac81d670ae5b62
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-09-23 04:26:23 +0900

    Fix wezterm setting

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,6 @@
 local wezterm = require 'wezterm';
+local act = wezterm.action
+
 return {
   use_ime=true,
   font = wezterm.font("Fira Code"),
@@ -16,4 +18,43 @@ return {
   initial_rows = 70,
   adjust_window_size_when_changing_font_size = true,
   native_macos_fullscreen_mode = true,
+  keys = {
+    -- This will create a new split and run the `top` program inside it
+    {
+      key = 'd',
+      mods = 'CMD|SHIFT',
+      action = wezterm.action.SplitPane {
+        direction = 'Left',
+        size = { Percent = 50 },
+      },
+    },
+    {
+      key = 'f',
+      mods = 'CMD|SHIFT',
+      action = wezterm.action.SplitPane {
+        direction = 'Down',
+        size = { Percent = 50 },
+      },
+    },
+    {
+      key = 'h',
+      mods = 'CTRL',
+      action = act.ActivatePaneDirection 'Left',
+    },
+    {
+      key = 'l',
+      mods = 'CTRL',
+      action = act.ActivatePaneDirection 'Right',
+    },
+    {
+      key = 'k',
+      mods = 'CTRL',
+      action = act.ActivatePaneDirection 'Up',
+    },
+    {
+      key = 'j',
+      mods = 'CTRL',
+      action = act.ActivatePaneDirection 'Down',
+    },
+  },
 }
