49724fac08292b7e01bf8a3316a5580638dfffe9
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-09-23 13:37:38 +0900

    Fix key config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -38,22 +38,22 @@ return {
     },
     {
       key = 'h',
-      mods = 'CTRL',
+      mods = 'CTRL|SHIFT',
       action = act.ActivatePaneDirection 'Left',
     },
     {
       key = 'l',
-      mods = 'CTRL',
+      mods = 'CTRL|SHIFT',
       action = act.ActivatePaneDirection 'Right',
     },
     {
       key = 'k',
-      mods = 'CTRL',
+      mods = 'CTRL|SHIFT',
       action = act.ActivatePaneDirection 'Up',
     },
     {
       key = 'j',
-      mods = 'CTRL',
+      mods = 'CTRL|SHIFT',
       action = act.ActivatePaneDirection 'Down',
     },
   },
