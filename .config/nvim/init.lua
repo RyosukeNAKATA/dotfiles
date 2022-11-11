f9b5090f291179079d322218d00460efb3471937
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-11-11 16:33:20 +0900

    Remove plenary & telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,22 +9,22 @@ local dein_dir = vim.env.HOME .. '/.cache/dein'
 local dein_repo_dir = dein_dir .. '/repos/github.com/Shougo/dein.vim'
 
 if not string.match(vim.o.runtimepath, '/dein.vim') then
-	if vim.fn.isdirectory(dein_repo_dir) ~= 1 then
-		os.execute('git clone https://github.com/Shougo/dein.vim '..dein_repo_dir)
-	end
-	vim.o.runtimepath = dein_repo_dir .. ',' .. vim.o.runtimepath 
+    if vim.fn.isdirectory(dein_repo_dir) ~= 1 then
+        os.execute('git clone https://github.com/Shougo/dein.vim ' .. dein_repo_dir)
+    end
+    vim.o.runtimepath = dein_repo_dir .. ',' .. vim.o.runtimepath
 end
 
 if vim.call('dein#load_state', dein_dir) == 1 then
-	local dein_toml_dir = vim.env.HOME .. '/dotfiles/.config/nvim'
-	local dein_toml = dein_toml_dir .. '/dein.toml'
-	local dein_toml_lazy = dein_toml_dir .. '/dein_lazy.toml'
+    local dein_toml_dir = vim.env.HOME .. '/dotfiles/.config/nvim'
+    local dein_toml = dein_toml_dir .. '/dein.toml'
+    local dein_toml_lazy = dein_toml_dir .. '/dein_lazy.toml'
 
-	vim.call('dein#begin', dein_dir, {vim.fn.expand('<sfile>'), dein_toml, dein_toml_lazy, dein_toml_input})
-	vim.call('dein#load_toml', dein_toml, {lazy = 0})
-	vim.call('dein#load_toml', dein_toml_lazy, {lazy = 1})
-	vim.call('dein#end')
-	vim.call('dein#save_state')
+    vim.call('dein#begin', dein_dir, { vim.fn.expand('<sfile>'), dein_toml, dein_toml_lazy, dein_toml_input })
+    vim.call('dein#load_toml', dein_toml, { lazy = 0 })
+    vim.call('dein#load_toml', dein_toml_lazy, { lazy = 1 })
+    vim.call('dein#end')
+    vim.call('dein#save_state')
 end
 
 -- load other settings
@@ -34,12 +34,12 @@ require('opt')
 
 vim.api.nvim_command [[colorscheme iceberg]]
 vim.api.nvim_create_autocmd(
-	"ColorScheme",
-	{ pattern = "*", command = "hi ColorColumn gui=reverse cterm=italic" }
+    "ColorScheme",
+    { pattern = "*", command = "hi ColorColumn gui=reverse cterm=italic" }
 )
 -- cofign for nvim-lastplace
-require'nvim-lastplace'.setup {
-    lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
-    lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+require 'nvim-lastplace'.setup {
+    lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+    lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
     lastplace_open_folds = true
 }
