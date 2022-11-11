345061b58a49faba3e75726df8094f5927469452
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-11-11 13:18:26 +0900

    fix

--- .config/nvim/plugins/nvim-treesitter.lua
+++ .config/nvim/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "python", "lua", "rust" },
+  ensure_installed = { "fish", "lua", "python", "rust", "markdown", "vim" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   -- sync_install = false,
@@ -35,8 +35,6 @@ require'nvim-treesitter.configs'.setup {
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
 	-- Setting colors
     colors = {
       -- Colors here
