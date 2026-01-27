51ac9f8bdb3c01a9b63800521c4a6c42e910d44e
RyosukeNAKATA <59600727+RyosukeNAKATA@users.noreply.github.com>
2026-01-27 12:43:14 +0900

    fix: error

--- dot_config/nvim/plugins/render-markdown.nvim.lua
+++ dot_config/nvim/plugins/render-markdown.nvim.lua
@@ -1,3 +1,10 @@
 require('render-markdown').setup({
     enabled = true,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'markdown',
+    callback = function()
+        vim.treesitter.start()
+    end,
+})
