e67ae8e7955a37041b03a6c5c00b448f81dc31eb
RyosukeNAKATA <59600727+RyosukeNAKATA@users.noreply.github.com>
2026-02-05 12:17:26 +0900

    fix(nvim): separate noice.nvim and nvim-navic configurations
    - Create proper noice.nvim.lua with cmdline disabled to avoid conflicts
      with ddc.vim cmdline completion
    - Rename misnamed config file to nvim-navic.lua
    - Fix dpp.toml hook_add references for both plugins
    
    This fixes the pum#popup#_open error when noice API was called without
    proper initialization.

--- dot_config/nvim/plugins/noice.nvim.lua
+++ dot_config/nvim/plugins/noice.nvim.lua
@@ -1,6 +1,30 @@
-require('nvim-navic').setup {
-    lsp = {
-        auto_attach = true,
+require("noice").setup({
+    -- Disable cmdline UI to avoid conflicts with ddc.vim cmdline completion
+    cmdline = {
+        enabled = false,
     },
-    highlight = true,
-}
+    -- Disable messages UI (optional, can be enabled if desired)
+    messages = {
+        enabled = true,
+    },
+    -- Disable popupmenu to avoid conflicts with pum.vim
+    popupmenu = {
+        enabled = false,
+    },
+    -- Keep notify for notifications
+    notify = {
+        enabled = true,
+    },
+    -- LSP settings
+    lsp = {
+        progress = {
+            enabled = true,
+        },
+        hover = {
+            enabled = true,
+        },
+        signature = {
+            enabled = false, -- Using denops-signature_help instead
+        },
+    },
+})
