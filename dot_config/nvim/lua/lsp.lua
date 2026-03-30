87521ab9b3a0d4849c2a545e57824db05e83f8ae
RyosukeNAKATA <59600727+RyosukeNAKATA@users.noreply.github.com>
2026-03-30 18:43:01 +0900

    fix Neovim startup issues and deprecated warnings

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client and client.supports_method("textDocument/formatting") then
+        if client and client:supports_method("textDocument/formatting") then
             vim.api.nvim_create_autocmd("BufWritePre", {
                 buffer = args.buf,
                 callback = function()
--- dot_config/nvim/plugins/nvim-colorizer.nvim.lua
+++ dot_config/nvim/plugins/nvim-colorizer.nvim.lua
@@ -5,4 +5,10 @@
 
 -- デフォルト設定で有効化
 -- CSS、HTML、Luaなどのファイルでカラーコードを視覚的に確認可能
+if vim.iter then
+    vim.tbl_flatten = function(t)
+        return vim.iter(t):flatten(math.huge):totable()
+    end
+end
+
 require 'colorizer'.setup()
